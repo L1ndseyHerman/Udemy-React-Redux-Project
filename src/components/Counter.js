92fbb7108632a81ee0609519d92bc57ba9bc51cd
L1ndseyHerman <lherman21@jcu.edu>
2021-09-18 18:18:13 -0400

    Caught up to vid234: linking the Counter component to the redux store so that it increments or decrements the number when the Counter buttons are clicked.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,35 @@
+import {useSelector, useDispatch} from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+
+  //  useDispatch returns a dispatch function.
+  const dispatch = useDispatch();
+
+  //  If u have a huge state object, just getting the counter property is helpful.
+  //  This also subscribes this component to the store! Helpful!
+  //  It updates the counter when the store updates... so the Observer pattern, maybe?
+  const counter = useSelector(state => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({type: 'increment'});
+  };
+
+  const decrementHandler = () => {
+    dispatch({type: 'decrement'});
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
