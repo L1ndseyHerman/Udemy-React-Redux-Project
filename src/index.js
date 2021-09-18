845c0011378720f49bfe401e6be658bd0b4af45b
L1ndseyHerman <lherman21@jcu.edu>
2021-09-18 17:56:03 -0400

    Caught up to vid232: made a Redux store and linked it to index.js.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//  New! Redux!
+import {Provider} from 'react-redux';
 
 import './index.css';
 import App from './App';
+//  New! Redux! 
+import store from './store/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//  U don't have to wrap the highest <App /> component, but only wrapped components will have 
+//  access to Redux.
+
+//  Make sure to set the pre-coded "store" prop!
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+document.getElementById('root'));
